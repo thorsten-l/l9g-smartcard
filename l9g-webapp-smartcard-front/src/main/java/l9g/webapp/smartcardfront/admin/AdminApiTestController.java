74238b13dfa7bd08911c6e337fe4211da12dc53c
kevin <kevin@10.21.12.85>
2025-02-27 14:54:06 +0100

    Added Signo Pad (Info File in templates/app)

--- l9g-webapp-smartcard-front/src/main/java/l9g/webapp/smartcardfront/admin/AdminApiTestController.java
+++ l9g-webapp-smartcard-front/src/main/java/l9g/webapp/smartcardfront/admin/AdminApiTestController.java
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+
 package l9g.webapp.smartcardfront.admin;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -33,6 +35,8 @@ import org.springframework.web.bind.annotation.PathVariable;
  *
  * @author Thorsten Ludewig (t.ludewig@gmail.com)
  */
+
+/*
 @Controller
 @Slf4j
 @RequiredArgsConstructor
@@ -88,3 +92,4 @@ public class AdminApiTestController
   }
 
 }
+*/
